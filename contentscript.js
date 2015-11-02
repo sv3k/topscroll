ae3d6e98b74cd0126cbc7bad45f87c567b6a308b
Anton Gorbunov <anton.gorbunov.ru@gmail.com>
2015-11-02 09:54:28 +0500

    Move JS stuff into separate namespace

--- contentscript.js
+++ contentscript.js
@@ -1,39 +1,37 @@
-function injectDiv() 
-{ 
-	var body = document.getElementsByTagName('body')[0];
-	var div = document.createElement('div');
+topScroll = {
 
-	var barId = document.createAttribute("id");
-	barId.value="topscroll-chrome-extension-bar";
-	div.setAttributeNode(barId);
-	div.onclick = topScroll;
-	div.oncontextmenu = bottomScroll;
-	
-	body.appendChild(div);
-}
-	
-function topScroll()
-{
-	if (window.pageYOffset === 0) {
-		window.scrollTo(window.pageXOffset, window.lastScrollPosition);
-		window.lastScrollPosition = 0;
-	} else {
-		window.lastScrollPosition = window.pageYOffset;
-		window.scrollTo(window.pageXOffset, 0);
-	}
-}
+	injectDiv: function() {
+		var body = document.getElementsByTagName('body')[0];
+		var div = document.createElement('div');
+		var barId = document.createAttribute("id");
+		barId.value = "topscroll-chrome-extension-bar";
+		div.setAttributeNode(barId);
+		div.onclick = topScroll.scrollUp;
+		div.oncontextmenu = topScroll.scrollDown;
+		body.appendChild(div);
+	},
 
-function bottomScroll()
-{
-	var height = document.body.clientHeight - window.innerHeight;
-	if (window.pageYOffset === height) {
-		window.scrollTo(window.pageXOffset, window.lastScrollPosition);
-		window.lastScrollPosition = height;
-	} else {
-		window.lastScrollPosition = window.pageYOffset;
-		window.scrollTo(window.pageXOffset, height);
+	scrollUp: function() {
+		if (window.pageYOffset === 0) {
+			window.scrollTo(window.pageXOffset, topScroll.lastScrollPosition);
+			topScroll.lastScrollPosition = 0;
+		} else {
+			topScroll.lastScrollPosition = window.pageYOffset;
+			window.scrollTo(window.pageXOffset, 0);
+		}
+	},
+
+	scrollDown: function() {
+		var height = document.body.clientHeight - window.innerHeight;
+		if (window.pageYOffset === height) {
+			window.scrollTo(window.pageXOffset, topScroll.lastScrollPosition);
+			topScroll.lastScrollPosition = height;
+		} else {
+			topScroll.lastScrollPosition = window.pageYOffset;
+			window.scrollTo(window.pageXOffset, height);
+		}
+		return false; // Prevent context menu appearing
 	}
-	return false; // Prevent context menu appearing
-}
+};
  	
-injectDiv();
+topScroll.injectDiv();
