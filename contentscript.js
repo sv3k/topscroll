bc830185eddf5f39d489cc01d3d5ac29b38d36cf
Anton Gorbunov <anton.gorbunov.ru@gmail.com>
2016-10-17 10:35:34 +0500

    Support more websites
    Fixes #2. If a page is not scrollable via default window.scrollTo
    — detect the biggest div on the page and scroll it instead.

--- contentscript.js
+++ contentscript.js
@@ -3,7 +3,7 @@ topScroll = {
 	injectDiv: function() {
 		var body = document.getElementsByTagName('body')[0];
 		var div = document.createElement('div');
-		var barId = document.createAttribute("id");
+		var barId = document.createAttribute('id');
 		barId.value = "topscroll-chrome-extension-bar";
 		div.setAttributeNode(barId);
 		div.onclick = topScroll.scrollUp;
@@ -12,42 +12,102 @@ topScroll = {
 	},
 
 	scrollUp: function() {
-		if (window.pageYOffset === 0) {
-			topScroll.scrollTo(topScroll.lastScrollPosition);
-			topScroll.lastScrollPosition = 0;
+		if (topScroll.target.getPosition() === 0) {
+			topScroll.scrollTo(topScroll.lastPosition);
+			topScroll.lastPosition = 0;
 		} else {
-			topScroll.lastScrollPosition = window.pageYOffset;
+			topScroll.lastPosition = topScroll.target.getPosition();
 			topScroll.scrollTo(0);
 		}
 	},
 
 	scrollDown: function() {
-		var bottomOffset = document.documentElement.scrollHeight - window.innerHeight;
-		if (window.pageYOffset === bottomOffset) {
-			topScroll.scrollTo(topScroll.lastScrollPosition);
-			topScroll.lastScrollPosition = bottomOffset;
+		var bottomPosition = topScroll.target.getBottomPosition();
+		if (topScroll.target.getPosition() === bottomPosition) {
+			topScroll.scrollTo(topScroll.lastPosition);
+			topScroll.lastPosition = bottomPosition;
 		} else {
-			topScroll.lastScrollPosition = window.pageYOffset;
-			topScroll.scrollTo(bottomOffset);
+			topScroll.lastPosition = topScroll.target.getPosition();
+			topScroll.scrollTo(bottomPosition);
 		}
 		return false; // Prevent context menu appearing
 	},
 
 	scrollTo: function(endY) {
 		var duration = 150;
-		var startY = window.scrollY;
+		var startY = topScroll.target.getPosition();
 		var distance = Math.max(endY,0) - startY;
 		var startTime = new Date().getTime();
-		(function loopScroll() {
+		(function smoothScroll() {
 			setTimeout(function () {
 				var p = Math.min((new Date().getTime() - startTime) / duration, 1); // Progress 0→1
 				var y = Math.max(Math.floor(startY + distance*(p < 0.5 ? 2*p*p : p*(4 - p*2)-1)), 0);
-				window.scrollTo(window.pageXOffset, y);
+				topScroll.target.setPosition(y);
 				if (p < 1) {
-					loopScroll();
+					smoothScroll();
 				}
 			}, 1)
 		})();
+	},
+
+	target: { // Undefined target state by default
+
+		findTarget: function() {
+			// Let's define target DOM element for scrolling
+			if (window.innerHeight === document.documentElement.scrollHeight) {
+				// Workaround for unscrollable root element, see #2
+				topScroll.target = {
+					div: topScroll.target.findBiggestDiv(),
+					getPosition: function() {
+						return this.div.scrollTop;
+					},
+					getBottomPosition: function() {
+						return this.div.scrollHeight - this.div.clientHeight;
+					},
+					setPosition: function(y) {
+						this.div.scrollTop = y;
+					}
+				};
+			} else {
+				topScroll.target = {
+					getPosition: function() {
+						return window.pageYOffset;
+					},
+					getBottomPosition: function() {
+						return document.documentElement.scrollHeight - window.innerHeight;
+					},
+					setPosition: function(y) {
+						window.scrollTo(window.pageXOffset, y);
+					}
+				};
+			}
+		},
+
+		findBiggestDiv: function() {
+			var items = document.getElementsByTagName('div');
+			var biggest = { scrollHeight: 0 };
+			for (var i = 0; i < items.length; i++) {
+			    if (items[i].scrollHeight > biggest.scrollHeight) {
+			    	biggest = items[i];
+			    }
+			}
+			return biggest;
+		},
+
+		getPosition: function() {
+			topScroll.target.findTarget();
+			return topScroll.target.getPosition();
+		},
+
+		getBottomPosition: function() {
+			topScroll.target.findTarget();
+			return topScroll.target.getBottomPosition();
+		},
+
+		setPosition: function(y) {
+			topScroll.target.findTarget();
+			topScroll.target.setPosition(y);
+		}
 	}
 };
 
