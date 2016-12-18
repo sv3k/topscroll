4b541489389c583e5c76d93d634d889859a11898
Anton Gorbunov <anton.gorbunov.ru@gmail.com>
2016-12-18 12:01:41 +0500

    Bump version

--- background.js
+++ background.js
@@ -1,16 +1,16 @@
 chrome.runtime.onInstalled.addListener(function(details) {
 	chrome.tabs.query({}, function(tabs) {
-        for (var i = 0; i < tabs.length; i++) {
-        	chrome.tabs.insertCSS(
-        		tabs[i].id,
-            	{ file: 'basic.css' }
-        	);
-            chrome.tabs.executeScript(
-            	tabs[i].id,
-            	{ file: 'contentscript.js' }
-            );
-        }
-    });
+		for (var i = 0; i < tabs.length; i++) {
+			chrome.tabs.insertCSS(
+				tabs[i].id,
+				{ file: 'basic.css' }
+			);
+			chrome.tabs.executeScript(
+				tabs[i].id,
+				{ file: 'contentscript.js' }
+			);
+		}
+	});
 });
 
 chrome.browserAction.onClicked.addListener(function() {
